4cfe1908517b60be3597e8d35a44081b6633f856
Nexrem <kristiana2000@abv.bg>
2021-05-18 00:37:20 +0300

    Drop rsvg crap

--- src/Wyvern.cs
+++ src/Wyvern.cs
@@ -32,54 +32,10 @@ namespace RSvg
         }
     }
 
-    public class GuiDialogAnnoyingText : GuiDialog
-    {
-        public override string ToggleKeyCombinationCode => "annoyingtextgui";
-        private LoadedTexture ownTexture;
-
-        public GuiDialogAnnoyingText(ICoreClientAPI capi) : base(capi)
-        {
-            SetupDialog();
-        }
-
-        private void SetupDialog()
-        {
-            Cairo.ImageSurface imageSurface = new Cairo.ImageSurface(Cairo.Format.Argb32, 200, 200);
-            Cairo.Context ctx = new Cairo.Context(imageSurface);
-
-            IAsset svg = capi.Assets.Get(new AssetLocation("testdomain", "textures/test.svg"));
-            System.Diagnostics.Debug.WriteLine(svg.Location.Path);
-
-            IntPtr error;
-            IntPtr handle = NativeMethods.rsvg_handle_new_from_data(svg.Data, svg.Data.Length, out error);
-            
-            if (error != IntPtr.Zero)
-            {
-                System.Diagnostics.Debug.WriteLine("FAILED");
-                throw new Win32Exception(Marshal.GetLastWin32Error());
-            }
-
-            NativeMethods.rsvg_handle_render_cairo(handle, ctx.Handle);
-
-            System.Diagnostics.Debug.WriteLine("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-
-            ownTexture = new LoadedTexture(capi, capi.Gui.LoadCairoTexture(imageSurface, true), 200, 200);
-        }
-
-        public override void OnRenderGUI(float deltaTime)
-        {
-            // Render2DLoadedTexture nicely takes the texture itself without the need to specify width and height
-            capi.Render.Render2DLoadedTexture(ownTexture, 100, 100, 9999);
-        }
-    }
-
-
     public class GuiShowPreview : GuiDialog
     {
         public override string ToggleKeyCombinationCode => "annoyingtextgui";
         private float counter = 0;
-
-        public override bool CaptureAllInputs() => true;
 
         public GuiShowPreview(ICoreClientAPI capi) : base(capi)
         {
@@ -268,7 +224,7 @@ namespace RSvg
         public LoadedTexture LargeTexture;
         private LoadedTexture TextTexture;
 
-        public void Recompose(ICoreClientAPI capi)
+        new public void Recompose(ICoreClientAPI capi)
         {
             this.TextTexture?.Dispose();
             this.TextTexture = new TextTextureUtil(capi).GenTextTexture(Lang.Get(this.Title), CairoFont.WhiteSmallText());
